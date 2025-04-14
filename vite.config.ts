d971a6fc89badad39e5651064c725edbbb04e641
Lance Lewandowski <lance0821@gmail.com>
2025-04-14 19:22:05 -0400

    Add Codecov bundler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { codecovVitePlugin } from '@codecov/vite-plugin';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(), // Put the Codecov vite plugin after all other plugins
+		codecovVitePlugin({
+			enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+			bundleName: process.env.CODECOV_BUNDLE_NAME,
+			uploadToken: process.env.CODECOV_TOKEN
+		})
+	],
 	test: {
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html'],
+			reportsDirectory: './coverage'
+		},
 		workspace: [
 			{
 				extends: './vite.config.ts',
